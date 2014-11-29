15bfc055a7e1013e1b78c38a74fea88ba5b3c3f1
vishnupriyan <vishnupriyanljcet@gmail.com>
2014-11-29 22:20:53 +0530

    Bug Fix

---
+++ gen/com/vishnu/samples/imageview/custom/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.vishnu.samples.imageview.custom;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ library/src/com/vishnu/samples/svg/SVGParseException.java
@@ -0,0 +1,17 @@
+package com.vishnu.samples.svg;
+
+
+public class SVGParseException extends RuntimeException {
+
+    public SVGParseException(String s) {
+        super(s);
+    }
+
+    public SVGParseException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public SVGParseException(Throwable throwable) {
+        super(throwable);
+    }
+}
